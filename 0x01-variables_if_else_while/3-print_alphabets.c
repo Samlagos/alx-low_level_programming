eb6cbcc635b9b1801af98c77b86c53dabf9cb978
Samlagos <nwachukwu132@outlook.com>
2023-06-17 04:59:00 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints the alphABET
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+char c;
+
+ch = 'a';
+c = 'A';
+	while
+       (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+	while
+	(c <= 'Z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Print all the letters except q and e
+ *
+ * Return: Zero 0 (Success)
+ */
+
+int main()
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+return (0);
+}
