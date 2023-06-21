2b6c5a712796db4793ed9740503441bf3daf2e44
Samlagos <nwachukwu132@outlook.com>
2023-06-21 13:24:34 -0700

    corrections

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* main - prints positive, negative or zero
+* @i: the number to be checked
+* Return: 0
+*/
+
+int main(int i)
+{
+        if (i < 0)
+	{
+	printf("%d is %s\n", i, negative);
+	}
+	else if (i > 0)
+	{
+	printf("%d is %s\n", i, positive);
+	}
+	else
+	{
+	printf("%d is %s\n", i, zero);
+	}
+        return (int i);
+}
