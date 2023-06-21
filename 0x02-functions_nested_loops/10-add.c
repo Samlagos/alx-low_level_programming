dfb3fbb484dc74d14508781bb1fe9abefbaf008a
Samlagos <nwachukwu132@outlook.com>
2023-06-21 02:20:31 -0700

    11-98

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _add - Entry point
+ *@n1: value to be computed
+ *@n2: value to be computed
+ * Return: Always 0 (Success)
+ */
+
+int _add(int n1, int n2)
+{
+return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Entry point
+ *@n: the number to start to print from
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (; n <= 98; n++)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
+}
+}
