98d35122cc48eed776c7acc2d04d9d857fa1f4d2
Samlagos <nwachukwu132@outlook.com>
2023-06-22 05:49:43 -0700

    1-isdigit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers
+ * @a: the number to be checked
+ * @b: the number to be checked
+ * Return: 1 if the function print two numbers and 0 for anything else
+ */
+
+int mul(int a, int b)
+{
+return (a + b);
+}
