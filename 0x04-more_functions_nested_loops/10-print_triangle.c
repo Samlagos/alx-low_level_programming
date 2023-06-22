854a9c5c0163130dcb7c0a347ba9034204c6ad2a
Samlagos <nwachukwu132@outlook.com>
2023-06-22 13:01:13 -0700

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - print_triangle with #
+ * @size: size
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+int x, y, z;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < size; x++)
+{
+for (y = size - x; y > 1; y--)
+{
+_putchar(' ');
+}
+for (z = 0; z <= x; z++);
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - FizzBuzz
+ * Return: void
+ */
+
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 15 == 0)
+	printf("fizzbuzz");
+else if (i % 3 == 0)
+	printf("fizz");
+else if (i % 5 == 0)
+	printf("fizzbuzz");
+else
+	printf("%d", i);
+if (i != 100)
+	printf(" ");
+}
+printf("\n");
+return (0);
+}
