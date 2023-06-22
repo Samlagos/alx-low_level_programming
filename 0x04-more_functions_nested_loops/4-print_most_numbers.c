8d16a89cc0075231fd350deb8f03eb0192e5628d
Samlagos <nwachukwu132@outlook.com>
2023-06-22 06:41:23 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9 exclude 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+if (n == 2 || n == 4)
+{
+continue;
+}
+else
+{
+_putchar(n + '0');
+}
+}
+_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14, 10 times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+int n;
+
+for ()
+}
