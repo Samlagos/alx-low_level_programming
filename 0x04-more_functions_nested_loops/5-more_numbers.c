71cec31e7121e1820ccfa39450950a8fa39ef9c3
Samlagos <nwachukwu132@outlook.com>
2023-06-22 10:47:51 -0700

    7-print_diagonal

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,7 +7,19 @@
 
 void more_numbers(void)
 {
-int n;
+int x;
+int y;
 
-for ()
+for (x = 0; x <= 10; x++)
+{
+for (y = 0; y <= 14; y++)
+{
+if (y > 9)
+{
+_putchar((y / 10) + '0');
+}
+_putchar((y % 10) + '0');
+}
+_putchar('\n');
+}
 }
