0064d29c6b8fb92163e5e38365d0d23f0bf04dec
Samlagos <nwachukwu132@outlook.com>
2023-07-09 01:24:56 -0700

    5-strstr.o:

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
  * _atoi - convert a string into an integer.
- *
  * @s: the string to use.
- *
  * Return: integer.
  */
 int _atoi(char *s)
@@ -18,7 +16,7 @@ sign *= -1;
 i++;
 }
 while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
-}
+{
 res = (res * 10) + (s[i] - '0');
 i++;
 }
--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -20,6 +20,6 @@ while (j < n)
 {
 dest[j] = '\0';
 j++;
-} 
+}
 return (dest);
 }
